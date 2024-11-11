c2a0ed8ad3ff4bf9422435bd1cd1ee06d6ba8cdb
Sabsteef <38842670+sabsteef@users.noreply.github.com>
2024-11-11 23:37:07 +0100

    Update racon_gateway.h

--- esphome/components/racon_gateway/racon_gateway.h
+++ esphome/components/racon_gateway/racon_gateway.h
@@ -2,6 +2,7 @@
 
 #include "esphome/core/component.h"
 #include "esphome/components/uart/uart.h"
+#include <WiFiUdp.h>
 #include "esphome/core/log.h"
 
 namespace esphome {
@@ -11,10 +12,10 @@ class RaconGateway : public Component, public uart::UARTDevice {
  public:
   explicit RaconGateway(uart::UARTComponent *parent) : uart::UARTDevice(parent) {}
   void setup() override;
-  void loop() override;
+  void send_and_read_data();  // Function to handle UART communication
 
  private:
-  std::string parse_data(const std::string &data);  // Parse data from serial
+  std::string parse_data(const std::string &data);
 };
 
 }  // namespace racon_gateway
